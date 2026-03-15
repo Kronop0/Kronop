d374557737eddbccc8d677e2e24c226c1952344f
Kronop <cloudkronop@gmail.com>
2026-03-15 21:28:17 +0530

    Update CameraComponent in live.jsa

--- components/upload/live.jsa/index.ts
+++ components/upload/live.jsa/index.ts
@@ -1,5 +1,8 @@
 // Export all components for easy import
-export { default as LiveStreamScreen } from './LiveStreamScreen';
+export { default as LiveUpload } from './LiveUpload';
+export { default as BroadcasterView } from './BroadcasterView';
+export { default as CameraComponent } from './CameraComponent';
 export { default as VideoPreview } from './VideoPreview';
 export { default as CommentOverlay } from './CommentOverlay';
 export { default as StreamInfo } from './StreamInfo';
+export { default as UserAccount } from './UserAccount';
