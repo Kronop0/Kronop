6c583d811c62b26903e164287569e39e8a3bf129
Kronop <cloudkronop@gmail.com>
2026-03-19 10:57:00 +0530

    Update configuration and dependencies

--- app.config.js
+++ app.config.js
@@ -1,4 +1,39 @@
-const appJson = require('./app.json');
+const appJson = {
+  expo: {
+    name: "kronop-app",
+    slug: "kronop-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "your-project-id"
+      }
+    }
+  }
+};
+
 require('dotenv').config();
 
 module.exports = () => ({
@@ -7,6 +42,7 @@ module.exports = () => ({
     '@react-native-google-signin/google-signin',
   ],
   extra: {
+    ...appJson.expo.extra,
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL || '',
     EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '',
     EXPO_PUBLIC_R2_ACCESS_KEY_ID: process.env.EXPO_PUBLIC_R2_ACCESS_KEY_ID || '',
