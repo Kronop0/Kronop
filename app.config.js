939791337fa9a31eab42a89a0d8637cd629d5bfa
Kronop <kronop@example.com>
2026-04-20 14:00:57 +0530

    Yahan apna message likhein (e.g., Initial Commit)

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ const appJson = {
     runtimeVersion: "1.0.0",
     updates: {
       enabled: true,
-      url: "https://u.expo.dev/92c8cf01-bba1-4123-80ff-3d6b7bfa0ab6"
+      url: "https://u.expo.dev/6e4f88ba-5a75-4cf2-9e58-5d67b5eec08f"
     },
     web: {
       favicon: "./assets/favicon.png"
